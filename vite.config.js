f58db5e592b0c1201da5b67b518b36e423f98c04
Amit Sharma <amit@neobrix.io>
2025-06-15 16:45:02 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/speak-to-krishna/',
   server: {
     allowedHosts: ['082b-117-245-250-113.ngrok-free.app'],
     host: true,
